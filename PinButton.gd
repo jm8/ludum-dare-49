ee66df232f09a3d265a50770cb6f5868c7d917f0
RebelAndroid <christophermcd0903@gmail.com>
2021-10-02 22:56:14 -0400

    added sound effects

--- PinButton.gd
+++ PinButton.gd
@@ -1,12 +1,5 @@
 extends TextureButton
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _process(delta):
 	if pressed:
 		rect_rotation = -45.0
